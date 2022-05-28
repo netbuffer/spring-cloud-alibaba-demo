b5c771ccb753000d71be836e86d4ef02ec0547b9
netbuffer <javawiki@163.com>
2022-05-28 17:23:49 +0800

    test nacos extension-configs

--- order-service/src/main/java/cn/netbuffer/springclouddemo/orderservice/api/OrderController.java
+++ order-service/src/main/java/cn/netbuffer/springclouddemo/orderservice/api/OrderController.java
@@ -3,10 +3,12 @@ package cn.netbuffer.springclouddemo.orderservice.api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 @Slf4j
 @RefreshScope
@@ -17,10 +19,19 @@ public class OrderController {
     @Value("${orderType:'common'}")
     private String orderType;
 
+    @Resource
+    private Environment environment;
+
     @GetMapping(value = "detail/{id}")
     public String getUser(@PathVariable("id") Long id) {
         log.debug("get order detail id={}", id);
         return orderType + ":" + id;
+    }
+
+    @GetMapping(value = "dictionaries")
+    public Object dictionaries(String key, String className) throws ClassNotFoundException {
+        log.debug("get dictionaries key={} className={}", key, className);
+        return environment.getProperty(key, Class.forName(className));
     }
 
 }
